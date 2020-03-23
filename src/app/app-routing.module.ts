2fdfec2d9031daf0bd4a396d9724165382ac5ac8
unknown <vitisme98@gmail.com>
2020-03-23 21:35:05 +0700

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { ProductAddComponent } from './product-add/product-add.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { ProductManagerComponent } from './product-manager/product-manager.component';
+import { ServiceComponent } from './service/service.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   {
@@ -16,6 +20,21 @@ const routes: Routes = [
   },
   { path: 'add', component: ProductAddComponent},
   { path: 'product/:id', component: ProductDetailComponent},
+  { path: 'home',
+    component: HomepageComponent
+  },
+  {
+    path: 'productmanager',
+    component: ProductManagerComponent
+  },
+  {
+    path: 'service',
+    component: ServiceComponent
+  },
+  {
+    path: 'ablout',
+    component: AboutComponent
+  }
 
 ];
 
